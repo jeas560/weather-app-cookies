588333a28926700d527ea60adebe3e9dde9e7de3
jeas560 <jeas560@gmail.com>
2023-12-27 10:32:07 -0300

    Initial refactoring version

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
 # weather-app-flask-cookies
-A project to study flask.
 
-After download the project, go to the directory and follow the code block:
+Projeto de estudo flask e utilização de cookies do browser.
 
-    virtualenv -p python3 .
-    source bin/activate
-    pip install -r requirements.txt
+## Objetivos da implementação
+
+- [x] Extração de dados da API da `Openweathermap`
+
+## 📦 Package manager
+
+Foi utilizado `poetry` como gerenciador de pacotes. Você pode instalar `poetry` seguindo as instruções [aqui](https://python-poetry.org/docs/#installation).
+
+Favor **Não** utilizar `pip` ou `conda` para instalar as dependencias. Para isso, utilize o seguinte comando:
+
+```bash
+poetry install
+```
+
+## Formatação de código com `black`
+
+Foi utilizado `black` para reformatar o código executando o seguinte comando:
+
+```bash
+black weather-app-flask-cookies 
+```
+
+## 🤖 Scripts de automação
+
+Em construção
+
+## 🧪 Testes
+
+Em construção
+
+## Inicializando o servidor
+
+Na pasta contendo o arquivo `app.py` rode o comando
+
     flask run
-
-
---
+++ weather-app-flask-cookies/app.py
@@ -0,0 +1,116 @@
+# from flask.helpers import url_for
+from dotenv import dotenv_values
+import requests
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    flash,
+    make_response,
+)
+
+app = Flask(__name__)
+
+config = dotenv_values(".env")
+app.config["SECRET_KEY"] = config["SECRET_KEY"]
+api_url = config["API_URL"]
+api_key = config["API_KEY"]
+
+
+def get_weather_data(city):
+    url = f"{api_url}{ city }&units=metric&appid={api_key}"
+    r = requests.get(url).json()
+    return r
+
+
+@app.route("/", methods=["GET"])
+def index_get():
+    if not request.cookies.get("accept"):
+        flash("Aceitando cookies", "cookie_active")
+
+    req = request.cookies.get("cidades")
+
+    if not req:
+        cities = [""]
+    else:
+        cities = req.split(", ")
+
+    weather_data = []
+
+    if cities != [""]:
+        for city in cities:
+            r = get_weather_data(city)
+
+            weather = {
+                "city": city,
+                "temperature": r["main"]["temp"],
+                "description": r["weather"][0]["description"],
+                "icon": r["weather"][0]["icon"],
+            }
+
+            weather_data.append(weather)
+
+    return render_template("weather.html", weather_data=weather_data)
+
+
+@app.route("/", methods=["POST"])
+def index_post():
+    req = request.cookies.get("cidades")
+
+    if not req:
+        cities = []
+    else:
+        cities = req.split(", ")
+
+    err_msg = ""
+
+    new_city = request.form.get("city").capitalize()
+
+    if new_city != "":
+        if new_city:
+            if new_city in cities:
+                err_msg = "Cidade já existente na base de dados!"
+            else:
+                new_city_data = get_weather_data(new_city)
+
+                if new_city_data["cod"] == 200:
+                    cities.append(new_city)
+                else:
+                    err_msg = "Cidade não encontrada!"
+        if err_msg:
+            flash(err_msg, "error")
+        else:
+            flash("Cidade adicionada com sucesso!", "succes")
+    else:
+        err_msg = "Inserir o nome da cidade no campo solicitado!"
+        flash(err_msg, "error")
+
+    res = make_response(redirect(url_for("index_get")))
+
+    if cities != []:
+        res.set_cookie("cidades", ", ".join(cities), max_age=60 * 60 * 24 * 365 * 2)
+
+    return res
+
+
+@app.route("/delete/<name>")
+def delete_city(name):
+    cities = request.cookies.get("cidades").split(", ")
+    cities.remove(name)
+
+    flash(f"{ name } eliminada da base de dados!")
+
+    res = make_response(redirect(url_for("index_get")))
+    res.set_cookie("cidades", ", ".join(cities), max_age=60 * 60 * 24 * 365 * 2)
+
+    return res
+
+
+@app.route("/cookie")
+def accept_cookies():
+    res = make_response(redirect(url_for("index_get")))
+    res.set_cookie("accept", "true", max_age=60 * 60 * 24 * 365 * 2)
+
+    return res
